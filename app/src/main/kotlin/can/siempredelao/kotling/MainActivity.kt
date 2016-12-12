e0425cc701cb8e90c30208be0cfe33b6f3177bd9
David Aguiar <siempredelao@gmail.com>
2016-12-12 10:28:24 +0100

    Kotlin variables

--- app/src/main/kotlin/can/siempredelao/kotling/MainActivity.kt
+++ app/src/main/kotlin/can/siempredelao/kotling/MainActivity.kt
@@ -1,12 +1,31 @@
 package can.siempredelao.kotling
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var x = 7 // "var" mutable
+        var y: String = "my String"
+        val z: View = findViewById(R.id.my_view) // "val" immutable
+
+        if (z is TextView) {
+            z.text = "I've been casted!"
+        }
+
+        // In Kotlin, everything is an object
+        val a: Int = 20
+        val b: Double = 21.5
+        val c: Unit = Unit
+
+        val d: Int = 20
+//        val e: Long = d // does not compile, unable to assign to a complex type, casting needed
+        val e: Long = d.toLong()
     }
 }
